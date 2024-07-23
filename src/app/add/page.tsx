ab6e76c134d50347d099520c4ebe6bfe9d394c82
mysoyul <vega2k_@naver.com>
2024-07-23 15:22:51 +0900

    add page 추가

---
+++ src/app/add/page.tsx
@@ -0,0 +1,12 @@
+import NavBar from "@/app/components/NavBar";
+
+const page = () => (
+  <div>
+    <NavBar />
+    <main>
+      <h1>New Bookmark Form</h1>
+    </main>
+  </div>
+);
+
+export default page;
