b40259e22a171c807aac3647a72ed38e762ee4c3
mysoyul <vega2k_@naver.com>
2024-07-23 16:22:26 +0900

    axios추가,types선언,async함수추가

--- src/app/bookmarks/page.tsx
+++ src/app/bookmarks/page.tsx
@@ -1,9 +1,21 @@
-import NavBar from "@/app/components/NavBar";
+import { getAllBookmarks } from "@/lib/bookmarkApi";
+import { BookmarkType, BookmarksResponse } from "@/types";
 
-export default function Home() {
+export default async function BookmarkPage()  {
+  const bookmarksData: Promise<BookmarksResponse> = getAllBookmarks()
+  const { data } = await bookmarksData
+  const bookmarks: BookmarkType[] = data
+
   return (
     <div>
         <h1>Welcome to Bookmarker</h1>
+        {
+          bookmarks.map(bookmark => {
+            return (
+                <h2 key={bookmark.id}>{bookmark.title}</h2>
+            )
+          })
+        }
     </div>
   );
 }
