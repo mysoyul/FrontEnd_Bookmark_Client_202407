55d1694f875c3b5a5c6f4c4810b54bee45c1b78e
mysoyul <vega2k_@naver.com>
2024-07-23 15:29:12 +0900

    layout 컴포넌트 수정

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -2,10 +2,7 @@ import NavBar from "@/app/components/NavBar";
 
 const page = () => (
   <div>
-    <NavBar />
-    <main>
       <h1>New Bookmark Form</h1>
-    </main>
   </div>
 );
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,7 @@ import NavBar from "@/app/components/NavBar";
 export default function Home() {
   return (
     <div>
-      <NavBar />
-      <main>
         <h1>Welcome to Bookmarker</h1>
-      </main>
     </div>
   );
 }
