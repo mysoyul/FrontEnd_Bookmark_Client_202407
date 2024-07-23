0a009c0293e721b3fa972058829001399e4c3278
mysoyul <vega2k_@naver.com>
2024-07-23 15:07:39 +0900

    FrontEnd Bookmark first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,35 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export default function Home() {
+  return (
+    <div>
+      <header>
+        <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+          <div className="container-fluid">
+            <a className="navbar-brand">Bookmarker</a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarCollapse"
+              aria-controls="navbarCollapse"
+              aria-expanded="false"
+              aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarCollapse">
+              <ul className="navbar-nav ms-auto">
+                <li className="nav-item">
+                  <a className="nav-link">Add Bookmark</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header>
+      <main>
+        <h1>Welcome to Bookmarker</h1>
+      </main>
+    </div>
+  );
+}
