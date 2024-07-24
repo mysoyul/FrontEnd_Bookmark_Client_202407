e0a55e1246212d2a6019c3f0236252a3257a4dfc
mysoyul <vega2k_@naver.com>
2024-07-24 09:49:48 +0900

    환경변수 .env 파일추가

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,19 @@ const nextConfig = {
                 permanent: true
             }
         ]
-    }
+    }, //redirects
+    
+    async rewrites() {
+        const API_BASE_URL = process.env.NEXT_PUBLIC_SERVER_API_BASE_URL
+        return [
+            {
+                source: '/api/:path*',
+                destination: `${API_BASE_URL}/api/:path*`
+                //'http://bookmarker-api-svc:8080/:path*/' // Proxy to Backend
+            }
+        ]
+    }, //rewrites  
+
 };
 
 export default nextConfig;
